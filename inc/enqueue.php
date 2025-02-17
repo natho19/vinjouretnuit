94acaf1743f51ca95f2cc6b2fcfa221005c04afe
Nathan Akpawu <nakpawu@gmail.com>
2025-02-17 17:50:35 +0000

    replace default jquery by template jquery

--- inc/enqueue.php
+++ inc/enqueue.php
@@ -4,6 +4,8 @@ function vinjouretnuit_register_assets() {
     wp_register_style('plugins', get_template_directory_uri() . '/assets/css/plugins.css');
     wp_register_style('style', get_template_directory_uri() . '/assets/css/style.css');
 
+    wp_register_script('vinjouretnuit-jquery', get_template_directory_uri() . '/assets/js/jquery-3.6.0.min.js', [], false, true);
+    wp_register_script('vinjouretnuit-jquery-migrate', get_template_directory_uri() . '/assets/js/jquery-migrate-3.0.0.min.js', [], false, true);
     wp_register_script('modernizr', get_template_directory_uri() . '/assets/js/modernizr-2.6.2.min.js', [], false, true);
     wp_register_script('imagesloaded', get_template_directory_uri() . '/assets/js/imagesloaded.pkgd.min.js', [], false, true);
     wp_register_script('jquery-isotope', get_template_directory_uri() . '/assets/js/jquery.isotope.v3.0.2.js', [], false, true);
@@ -19,12 +21,13 @@ function vinjouretnuit_register_assets() {
     wp_register_script('select2', get_template_directory_uri() . '/assets/js/select2.js', [], false, true);
     wp_register_script('datepicker', get_template_directory_uri() . '/assets/js/datepicker.js', [], false, true);
     wp_register_script('scroll', get_template_directory_uri() . '/assets/js/smooth-scroll.min.js', [], false, true);
-    wp_register_script('script', get_template_directory_uri() . '/assets/js/script.js', [], false, true);
+    wp_register_script('custom', get_template_directory_uri() . '/assets/js/custom.js', [], false, true);
     
     wp_enqueue_style('plugins');
     wp_enqueue_style('style');
     
-    wp_enqueue_script('jquery');
+    wp_enqueue_script('vinjouretnuit-jquery');
+    wp_enqueue_script('vinjouretnuit-jquery-migrate');
     wp_enqueue_script('modernizr');
     wp_enqueue_script('imagesloaded');
     wp_enqueue_script('jquery-isotope');
@@ -40,7 +43,7 @@ function vinjouretnuit_register_assets() {
     wp_enqueue_script('select2');
     wp_enqueue_script('datepicker');
     wp_enqueue_script('scroll');
-    wp_enqueue_script('script');
+    wp_enqueue_script('custom');
 }
 
 add_action('wp_enqueue_scripts', 'vinjouretnuit_register_assets');
