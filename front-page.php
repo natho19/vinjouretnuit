707093196d7b867989e5b0e46d8d77064e1f1349
Nathan Akpawu <nakpawu@gmail.com>
2025-02-21 18:58:32 +0000

    static homepage template

---
+++ front-page.php
@@ -0,0 +1,102 @@
+<?php get_header() ?>
+
+<!-- Header Banner / full-height -->
+<div class="banner-header full-height valign bg-img bg-fixed" data-overlay-dark="6" data-background="<?= VJN_IMG_URL . '/slider/3.jpg'; ?>">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12 text-center">
+                <div class="v-middle caption">
+                    <h1>Vin Jour et Nuit <span>Lorem ipsum dolor</span></h1>
+                    <a href="#" data-scroll-nav="1" class="button-1 mt-20">Découvrir nos vins</a>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- arrow down -->
+    <div class="arrow bounce text-center">
+        <a href="#" data-scroll-nav="1" class=""> <i class="ti-arrow-down"></i> </a>
+    </div>
+</div>
+
+<!-- Services -->
+<section class="services-1 section-padding bg-cream" data-scroll-index="1">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="section-head text-center">
+                    <div class="section-title">Nos services</div>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-4">
+                <div class="item">
+                    <span class="flaticon-hamburger-1"></span>
+                    <h5>Cheeseburger</h5>
+                    <p>Restaurant vulputate massa ons amet ravida haretra nuam the drana miss uctus enec accumsan aliquam sit sapien.</p>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="item">
+                    <span class="flaticon-pizza-1"></span>
+                    <h5>Pizza Slice</h5>
+                    <p>Restaurant vulputate massa ons amet ravida haretra nuam the drana miss uctus enec accumsan aliquam sit sapien.</p>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="item">
+                    <span class="flaticon-coffee-cup"></span>
+                    <h5>Coffee-Cup</h5>
+                    <p>Restaurant vulputate massa ons amet ravida haretra nuam the drana miss uctus enec accumsan aliquam sit sapien.</p>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="item">
+                    <span class="flaticon-chicken-leg"></span>
+                    <h5>Chicken-Leg</h5>
+                    <p>Restaurant vulputate massa ons amet ravida haretra nuam the drana miss uctus enec accumsan aliquam sit sapien.</p>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="item">
+                    <span class="flaticon-fish"></span>
+                    <h5>Fish</h5>
+                    <p>Restaurant vulputate massa ons amet ravida haretra nuam the drana miss uctus enec accumsan aliquam sit sapien.</p>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="item">
+                    <span class="flaticon-wedding-cake"></span>
+                    <h5>Wedding Cake</h5>
+                    <p>Restaurant vulputate massa ons amet ravida haretra nuam the drana miss uctus enec accumsan aliquam sit sapien.</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<!-- Restaurant Video -->
+<section class="section-padding video-wrapper video bg-img bg-fixed" data-overlay-dark="6" data-background="<?= VJN_IMG_URL . '/banner2.jpg' ?>">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="section-head text-center">
+                    <div class="section-title white">Regarder notre vidéo</div>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-12 text-center">
+                <a class="vid" href="https://www.youtube.com/watch?v=3l_2NTf6-Io">
+                    <div class="vid-butn">
+                        <span class="icon">
+                            <i class="ti-control-play"></i>
+                        </span>
+                    </div>
+                </a>
+            </div>
+        </div>
+    </div>
+</section>
+
+<?php get_footer() ?>
--- header.php
+++ header.php
@@ -19,28 +19,30 @@
     </div>
 
     <!-- Navbar -->
-    <nav class="navbar navbar-expand-lg">
-        <div class="container">
-            <!-- Logo -->
-            <div class="logo-wrapper">
-                <a class="logo" href="http://localhost/vinjouretnuit/">
-                    <img src="<?= VJN_IMG_URL . '/logo.png' ?>" class="logo-img" alt="Logo">
-                </a>
+    <header>
+        <nav class="navbar navbar-expand-lg">
+            <div class="container">
+                <!-- Logo -->
+                <div class="logo-wrapper">
+                    <a class="logo" href="http://localhost/vinjouretnuit/">
+                        <img src="<?= VJN_IMG_URL . '/logo.png' ?>" class="logo-img" alt="Logo">
+                    </a>
+                </div>
+                <!-- Button -->
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbar" aria-controls="navbar" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon">
+                    <i class="ti-menu"></i></span>
+                </button>
+                <!-- Menu -->
+                <div class="collapse navbar-collapse" id="navbar">
+                    <ul class="navbar-nav ms-auto">
+                        <li class="nav-item"><a class="nav-link" href="http://localhost/vinjouretnuit/">Accueil</a></li>
+                        <li class="nav-item"><a class="nav-link" href="http://localhost/vinjouretnuit/a-propos/">A propos</a></li>
+                        <li class="nav-item"><a class="nav-link" href="http://localhost/vinjouretnuit/nos-vins/">Nos vins</a></li>
+                        <li class="nav-item"><a class="nav-link" href="http://localhost/vinjouretnuit/blog/">Blog</a></li>
+                        <li class="nav-item"><a class="nav-link" href="http://localhost/vinjouretnuit/contact/">Contact</a></li>
+                    </ul>
+                </div>
             </div>
-            <!-- Button -->
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbar" aria-controls="navbar" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon">
-                <i class="ti-menu"></i></span>
-            </button>
-            <!-- Menu -->
-            <div class="collapse navbar-collapse" id="navbar">
-                <ul class="navbar-nav ms-auto">
-                    <li class="nav-item"><a class="nav-link" href="http://localhost/vinjouretnuit/">Accueil</a></li>
-                    <li class="nav-item"><a class="nav-link" href="http://localhost/vinjouretnuit/a-propos/">A propos</a></li>
-                    <li class="nav-item"><a class="nav-link" href="http://localhost/vinjouretnuit/nos-vins/">Nos vins</a></li>
-                    <li class="nav-item"><a class="nav-link" href="http://localhost/vinjouretnuit/blog/">Blog</a></li>
-                    <li class="nav-item"><a class="nav-link" href="http://localhost/vinjouretnuit/contact/">Contact</a></li>
-                </ul>
-            </div>
-        </div>
-    </nav>
+        </nav>
+    </header>
